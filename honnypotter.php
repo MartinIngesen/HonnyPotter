85008382a01900600f26d94b868374c8ae201502
Martin Ingesen <martin@ingesen.no>
2015-10-22 01:51:59 +0200

    added additional information for WordPress to pick up on.

--- honnypotter.php
+++ honnypotter.php
@@ -1,7 +1,33 @@
 <?php
 /*
 Plugin Name: HonnyPotter
+Plugin URI: https://github.com/MartinIngesen/HonnyPotter
+Description: Log all failed login-attempts.
+Version: 1.1
+Author: Martin ingesen
+Author URI: http://martin.ingesen.no
+License: GPLv2 or later
+Text Domain: honnypotter
 */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Martin Ingesen.
+*/
+
 require_once (plugin_dir_path(__FILE__) . 'options.php');
 
 register_activation_hook(__FILE__, 'options_init');
